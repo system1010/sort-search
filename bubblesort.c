aa3d0202a09e989176c94ce88ad1eff05a77d09d
system <32022861+system1010@users.noreply.github.com>
2018-04-21 11:56:36 +0500

    Create bubblesort.c
    We Had To Use Dark Magic To Use Make This Work

---
+++ bubblesort.c
@@ -0,0 +1,34 @@
+ #include <string.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+
+
+ /* bubbles sort. */
+ void bubble(char *items, int count)
+ {
+   register int a, b;
+   register char t;
+
+   for(a=1; a < count; ++a)
+     for(b=count-1; b >= a; --b) {
+       if(items[b-1] > items[b]) {
+         /* exchange elements */
+         t = items[b-1];
+         items[b-1] = items[b];
+         items[b] = t;
+       }
+     }
+ }
+
+
+ int main(void)
+ {
+   char s[255];
+
+   printf("input string:");
+   gets(s);
+   bubble(s, strlen(s));
+   printf("sorted string: %s.\n", s);
+
+   return 0;
+ }
