0bf93188c35c32ba3d0f1a926749c6afc2820419
system <32022861+system1010@users.noreply.github.com>
2018-07-05 21:48:00 +0500

    inOrder
    inOrder tree stack

---
+++ inOrder.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node 
+{
+char data;
+struct Node *left, *right;
+};
+
+struct Node* newNode(char data)
+{
+	struct Node* Node = (struct Node*)malloc(sizeof(struct Node));
+
+        Node->data = data;
+        Node->left = NULL;
+        Node->right = NULL;
+
+        return(Node);
+}
+
+struct Stack
+{
+	int top;
+	struct Node* array[100];
+};
+
+struct Stack* createStack(unsigned capacity)
+{
+struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack));
+stack->top = -1;
+return stack;
+}
+void push(struct Stack* stack, struct Node* item)
+{
+stack->array[++stack->top] = item;
+}
+struct Node* pop(struct Stack* stack)
+{
+return stack->array[stack->top--];
+}
+
+int main()
+{
+
+struct Node* root=newNode('A');
+root->left=newNode('B');
+root->right=newNode('C');
+root->left->left=newNode('D');
+root->right->left=newNode('E');
+root->right->right=newNode('F');
+root->right->left->right=newNode('G');
+root->right->right->left=newNode('H');
+root->right->right->right=newNode('J');
+
+struct Stack* stack = createStack(100);
+
+struct Node* P=newNode('P');;
+//T1
+P=root;
+
+int x=-1;
+while(1){
+	//T2
+	if(P!=NULL){
+		//T3
+		push(stack, P);
+		P=P->left;
+	}else{
+		//T4
+		if(stack->top>=0){
+		P=pop(stack);
+		//T5
+		printf("%c\n", P->data);
+		P=P->right;
+		}else break;
+
+	}
+}       
+
+return 0;
+}
