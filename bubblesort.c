c34092a319077418523ea9df31ed8ddb83788a15
system <ali_expres@list.ru>
2018-11-10 19:15:02 +0500

    I will not apologize for art.

--- bubblesort.c
+++ bubblesort.c
@@ -1,45 +1,25 @@
 #include <stdio.h> 
-#include <time.h>
-void move(int *xp, int *yp) 
-{ 
-	int temp = *xp; 
-	*xp = *yp; 
-	*yp = temp; 
-} 
-clock_t t,t1,t2,t3;
-void printArray(int arr[], int size) 
-{ 
-	for (int i=0; i < size; i++) 
-		printf("%d ", arr[i]); 
-	printf("n"); 
-} 
 int main() 
 { 
 	int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
 	int n = sizeof(arr)/sizeof(arr[0]); 
-	t3=clock(); 
-	printf("%f n=%i ", ((double)t1)/CLOCKS_PER_SEC, n);
-	printArray(arr, n);
-	printf("\n");
-	int i, j; 
-	for (i = 0; i < n-1; i++){       
-	for (j = 0; j < n-i-1; j++) 
-	if (arr[j] > arr[j+1]){ 
-	t1=clock(); 
-	printf("%f i=%i j=%i %i > %i ", ((double)t1)/CLOCKS_PER_SEC,i,j ,arr[j], arr[j+1]);
-	printf("inversion, moving up: ");
-	move(&arr[j], &arr[j+1]); 
-	printArray(arr,n);
-	printf("\n");   
-	}else {
-	t2=clock(); 
-	printf("%f i-%i j=%i %i < %i ", ((double)t2)/CLOCKS_PER_SEC,i,j, arr[j], arr[j+1]);
-	printf("not inversion :       ");       
-	printArray(arr,n);
-	printf("\n");}} 
-	t=clock(); 
-	printf("%f ", ((double)t)/CLOCKS_PER_SEC);
-	printf("Sorted array: "); 
-	printArray(arr, n); 
-	return 0; 
-} 
+B1:;	int bound = n, t;
+   	
+B2:;	t=0;
+   	for (int j = 0; j < bound-1; j++){	   
+B3:;	if (arr[j] > arr[j+1]){ 
+	int temp = arr[j];
+	arr[j] = arr[j+1]; 
+	arr[j+1] = temp;	
+   	t=j;
+   	}
+   	}
+B4:;   	if (t==0) {
+	for (int i=0; i < n; i++) 
+		printf("%d ", arr[i]); 
+	printf("\n"); 
+	return 0;}
+   	bound=t+1;
+	goto B2;
+}	
+
