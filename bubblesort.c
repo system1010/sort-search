fcb67fd79383df9c12032c73bd6d153b8d482eed
system <ali_expres@list.ru>
2018-11-11 16:22:01 +0500

    cool

--- bubblesort.c
+++ bubblesort.c
@@ -1,6 +1,26 @@
 #include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
-clock_t t1,t2,t3;
+
+#define NANO 1000000000L
+
+// buf needs to store 30 characters
+int timespec2str(char *buf, uint len, struct timespec *ts) {
+int ret;
+struct tm t;
+tzset();
+if (localtime_r(&(ts->tv_sec), &t) == NULL)
+return 1;
+ret = strftime(buf, len, "%F %T", &t);
+if (ret == 0)
+return 2;
+len -= ret - 1;
+ret = snprintf(&buf[strlen(buf)], len, ".%09ld", ts->tv_nsec);
+if (ret >= len)
+return 3;
+return 0;
+}
 int main() 
 { 
 	int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
@@ -8,6 +28,17 @@ int main()
 	for (int i=0; i < n; i++) 
 		printf("%d ", arr[i]); 
 	printf("\n"); 
+
+unsigned long resol;
+clockid_t clk_id = CLOCK_REALTIME;
+const uint TIME_FMT = strlen("2012-12-31 12:59:59.123456789") + 1;
+char timestr[TIME_FMT];
+struct timespec ts, res;
+clock_getres(clk_id, &res);
+clock_gettime(clk_id, &ts);
+timespec2str(timestr, sizeof(timestr), &ts);
+resol = res.tv_sec * NANO + res.tv_nsec;
+
 B1:;	int bound = n, t;
    	
 B2:;	t=0;
@@ -17,8 +48,14 @@ B3:;	if (arr[j] > arr[j+1]){
 	arr[j] = arr[j+1]; 
 	arr[j+1] = temp;	
    	t=j;
-	t1=clock(); 
-	printf("%f j=%i %i > %i \n", ((double)t1)/CLOCKS_PER_SEC,j ,arr[j], arr[j+1]);
+
+resol = res.tv_sec * NANO + res.tv_nsec;
+
+	printf("%s ",timestr);
+	 for (int i=0; i < n; i++) 
+		                 printf("%d ", arr[i]); 
+	         printf("\n"); 
+
    	}
    	}
 B4:;   	if (t==0) {
