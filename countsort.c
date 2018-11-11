d4a491514fb42ffe178ae83468f56a5d1e19ac88
system <ali_expres@list.ru>
2018-11-11 17:50:58 +0500

    cool

--- countsort.c
+++ countsort.c
@@ -1,31 +1,69 @@
 #include <stdio.h> 
-#include <time.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define NANO 1000000000L
+
+// buf needs to store 30 characters
+int timespec2str(char *buf, uint len, struct timespec *ts) {
+int ret;
+struct tm t;
+tzset();
+if (localtime_r(&(ts->tv_sec), &t) == NULL)
+return 1;
+ret = strftime(buf, len, "%F %T", &t);
+if (ret == 0)
+return 2;
+len -= ret - 1;
+ret = snprintf(&buf[strlen(buf)], len, ".%09ld", ts->tv_nsec);
+if (ret >= len)
+return 3;
+return 0;
+}
 int signum(int a, int b){
 return (a-b > 0) ? 1 : ((a-b < 0) ? -1 : 0);
 }
 int main(){ 
 	int arr[] = {170, 45, 75, 90, 802, 24, 2, 66, 46, 51, 765, 665, 33, 7, 39};
+	clockid_t clk_id = CLOCK_REALTIME;
+	const uint TIME_FMT = strlen("2012-12-31 12:59:59.123456789") + 1;
+	char timestr[TIME_FMT];
+	struct timespec ts, res;
+	clock_getres(clk_id, &res);
+	clock_gettime(clk_id, &ts);
+	timespec2str(timestr, sizeof(timestr), &ts);
+		unsigned long resol = res.tv_sec * NANO + res.tv_nsec;
+		printf("%s\n", timestr);
+
+
+
 
   	int n = sizeof(arr)/sizeof(arr[0]);
 	int output[n]; 
         int count[sizeof(arr)/sizeof(arr[0])]={0}, i, j; 
-	clock_t t,t2,t3;
-	t = clock();
 	      
 	for(i = n-1;i>=1; --i) 
 	for (j=i-1;j>=0;--j){
-	t3=clock()-t; 
 	if (signum(arr[i], arr[j])==1)
 	{
 	++count[i];
-	printf("%f i=%i j=%i %i > %i", ((double)t3)/CLOCKS_PER_SEC,i,j, arr[i],arr[j]);
+    clock_gettime(clk_id, &ts);
+            timespec2str(timestr, sizeof(timestr), &ts);
+		                    unsigned long resol = res.tv_sec * NANO + res.tv_nsec;
+	                    printf("%s\n", timestr);
+
+	printf("i=%i j=%i %i > %i",i,j, arr[i],arr[j]);
 	for (int k = 0; k < n; ++k)
 	printf(" %i",count[k]); 
 	printf("  inversion count i\n");
 	}else{
 	++count[j];
-	printf("%f i=%i j=%i %i < %i", ((double)t3)/CLOCKS_PER_SEC,i,j, arr[i],arr[j]);
+	    clock_gettime(clk_id, &ts);
+	            timespec2str(timestr, sizeof(timestr), &ts);
+			                    unsigned long resol = res.tv_sec * NANO + res.tv_nsec;
+		                    printf("%s\n", timestr);
+	printf("i=%i j=%i %i < %i",i,j, arr[i],arr[j]);
 	for (int k = 0; k < n; ++k)
 	printf(" %i",count[k]);
 	printf("  not inversion count j\n");
@@ -37,8 +75,5 @@ int main(){
 	for (i=0;i<n;++i)
 		printf("%i ", output[i]);
         printf("\n");
-	t2=clock()-t; 
-	printf("sec: %f ", ((double)t2)/CLOCKS_PER_SEC);
-	printf("end\n");
 	return 0; 
 } 
