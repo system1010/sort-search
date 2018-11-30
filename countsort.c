4b2ac58daf52d83452de704688704546dc119e57
system <ali_expres@list.ru>
2018-11-30 19:20:43 +0500

    forgot to save that file

--- countsort.c
+++ countsort.c
@@ -35,8 +35,8 @@ C2:;	for(i = n-1;i>=1; --i)
 	for (i = 0; i < n; ++i)
 	output[count[i]] = arr[i]; 
 	
-	//for (i=0;i<n;++i)
-	//	printf("%i ", output[i]);
-        //printf("\n");
+	for (i=0;i<n;++i)
+		printf("%i ", output[i]);
+        printf("\n");
 	return 0; 
 } 
