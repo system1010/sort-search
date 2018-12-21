63e521d9b069c95413f808fe0e6af6838de82cc7
system <ali_expres@list.ru>
2018-12-21 16:28:57 +0500

    done

--- cocktailsort.c
+++ cocktailsort.c
@@ -1,49 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int *n1, int *n2)
-{
-	int aux;
-	aux = *n1;
-	*n1 = *n2;
-	*n2 = aux;
-}
-
-void cocktail_sort(int *v, int n)
-{
-	int i, j, gatilho, k;
+int main() 
+{ 
+	int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+        int i, j, gatilho, k;
 	do {
-	for(i = 0; i < n/2; i++)
+        for(i = 0; i < n/2; i++)
+        {
+        gatilho = 0;
+        for(j = i; j < n-i-1; j++)
+        {
+        if(arr[j] >arr[j+1])
+        {
+        int temp = arr[j];
+        arr[j] = arr[j+1]; 
+        arr[j+1] = temp;   
+        gatilho = 1;
+        }
+        }
+	for(k = n-2-i; k > i; k--)
 	{
-		gatilho = 0;
-		for(j = i; j < n-i-1; j++)
-		{
-			if(*(v+j) > *(v+j+1))
-			{
-				swap(v+j, v+j+1);
-				gatilho = 1;
-			}
-		}
-		for(k = n-2-i; k > i; k--)
-		{
-			if(*(v+k) < *(v+k-1))
-			{
-				swap(v+k, v+k-1);
-				gatilho = 1;
-			}
-		}
-
+	if(arr[k] < arr[k-1])
+	{
+	int temp = arr[k];
+	arr[k] = arr[k-1]; 
+	arr[k-1] = temp;   
+	gatilho = 1;
 	}
-} while(gatilho == 1);
-
-}
-
-void main(){
-int arr[]={7,5,3,8,5,2,9};
-int n = 7;
-cocktail_sort(*arr,n);
-
-
+	}
+        }
+	} while(gatilho == 1);
+	for (int i=0; i < n; i++) 
+        printf("%d ", arr[i]); 
+        printf("\n"); 
 
 
 return 0;
